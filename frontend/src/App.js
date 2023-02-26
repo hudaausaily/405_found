f6cf8850fcf870dc392baf994131158ec651d8e3
Ahmadzytoon <ahmadzytoon@outlook.com>
2023-02-26 22:42:59 +0300

    ahmad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 
+
 import Home from './pages/home';
 import Register from './pages/register';
 import Login from './pages/login';
-import Profile from './pages/profile';
-import SingleGroup from './pages/singleGroup';
-import React from 'react';
-import Navbar from './components/navbar';
-import Nav from './components/nav';
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div >
-     <Home/>
-    </div>
+    <Routes>
+    <Route path="/home" element={ <Home/> } />
+    <Route path="/" element={ <Login/> } />
+    <Route path="/register" element={ <Register/> } />
+
+  </Routes>
   );
 }
 
